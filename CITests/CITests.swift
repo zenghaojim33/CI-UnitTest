d21078734b364c20cf9837569679e03f82126d3f
anson <anson@zoeker.cn>
2017-08-25 17:41:58 +0800

    3

--- CITests/CITests.swift
+++ CITests/CITests.swift
@@ -25,7 +25,6 @@ class CITests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertTrue(false)
     }
     
     func testPerformanceExample() {
